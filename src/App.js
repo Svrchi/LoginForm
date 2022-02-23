4bd997ed338096e0c6995778788d86d373770633
svrchi <kevinsarchi@yahoo.com>
2022-02-23 17:19:45 -0500

    style and functionality adjustments

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import './App.css';
 import Form from './Components/Form/Form';
 
+
+
 function App() {
   return (
     <div className="App">
--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import './Form.css';
-import undraw from '../../imgs/undraw.svg'
+import undraw from '../../imgs/undraw.svg';
 
 // we need to check that email is a valid email
 // we cannot submit form if password is empty or less than 1 character
@@ -8,107 +8,131 @@ import undraw from '../../imgs/undraw.svg'
 const Form = () => {
   // EMAIL STATE
   const [email, setEmail] = useState('');
-  const [emailIsNotValid, setEmailIsNotValid] = useState({
-    isValid: false,
-    message: ''
+  const [emailValid, setEmailValid] = useState({
+    isValid: true
   });
   // PASSWORD STATE
   const [password, setPassword] = useState('');
-  const [passwordIsNotValid, setPasswordIsNotValid] = useState({
-    isValid: false,
-    message: ''
+  const [passwordValid, setPasswordValid] = useState({
+    isValid: true
   });
 
+
+
   // FUNCTIONALITY
-  //   async const handleSubmit = () => {
-  //     console.log('on submit');
-  //     fetch('https://reqres.in/api/users'),{
-
-  //     }
-  //   };
-
-  const sendRequest = (e) => {
-    e.preventDefault();
-
-    fetch('https://reqres.in//api/login', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        email: { email },
-        password: { password }
-      })
-    }).then(res => {
-      return res.json
-    })
-      .then(data => console.log(data))
-      .catch(error => console.log("ERROR"))
-  }
-
-  const asyncPostCall = async () => {
-    try {
-      const response = await fetch('https://reqres.in//api/login', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-          // your expected POST request payload goes here
-          email: { email },
-          password: { password }
-        })
-      });
-      const data = await response.json();
-      // enter you logic when the fetch is successful
-      console.log(data);
-    } catch (error) {
-      // enter your logic for when there is an error (ex. error toast)
-      console.log(error)
-    }
-  }
-
-
   const validateEmail = (e) => {
-    console.log('in email validate');
-    setEmail(e.target.value);
-    console.log('current email', email);
+    if (e.target.value === 'test@test.com') {
+      console.log(`EMAIL IS VALIDATED`);
+      setEmail(e.target.value);
+      setEmailValid({
+        isValid: true
+      });
+    } else {
+      setEmailValid({
+        isValid: false
+      });
+    }
+  };
+
+  const validatePassword = (e) => {
+    if (e.target.value === 'test') {
+      console.log(`PASSWORD IS VALIDATED`);
+      setPassword(e.target.value);
+      setPasswordValid({
+        isValid: true
+      });
+    } else {
+      setPasswordValid({
+        isValid: false
+      });
+    }
   };
 
 
+  const formReset = () => {
+    // when submitted the component rerenders resetting the default vaule
+    // form reset not neccessary ?
+    setEmail('');
+    setEmailValid({ isValid: true });
+    setPassword('');
+    setPasswordValid({ isValid: true });
+    console.log(
+      `FORM RESET: ${email} ${password} ${emailValid} ${passwordValid}`
+    );
+  };
 
-  console.log('Email:', email)
-  console.log("Password:", password)
+  const asyncPostCall = async (e) => {
+    // e.preventDefault();
+    console.log(`SUBMITTED EMAIL: ${email} PASSWORD: ${password}`);
+    // try {
+    //   const response = await fetch('https://reqres.in//api/login', {
+    //     method: 'POST',
+    //     headers: {
+    //       'Content-Type': 'application/json'
+    //     },
+    //     body: JSON.stringify({
+    //       // your expected POST request payload goes here
+    //       email: { email },
+    //       password: { password }
+    //     })
+    //   });
+    //   const data = await response.json();
+    //   // enter you logic when the fetch is successful
+    //   console.log(data);
+    // } catch (error) {
+    //   // enter your logic for when there is an error (ex. error toast)
+    //   console.log(error)
+    // }
+    // console.log('onsubmit:', email)
+    // formReset();
+  };
+
+  // INPUT ERROR MESSAGES
+  const renderEmailError = () => {
+    return emailValid.isValid ? <br /> : <p>incorrect email input </p>;
+  };
+
+  const renderPasswordError = () => {
+    return passwordValid.isValid ? <br /> : <p>incorrect password input </p>;
+  };
+
+  console.log(`email: ${email}`);
 
   return (
-    <div className='wrapper'>
-      <img className='svg' src={undraw} />
-      <div className='formContainer'>
-        <div className='header_content'>
-        <p>Login</p>
+    <div className="wrapper">
+      <img className="svg" src={undraw} />
+      <div className="formContainer">
+        <div className="header_content">
+          <p>Login</p>
         </div>
-        <form className="form" onSubmit={(e) => sendRequest(e)}>
-          {/* EMAIL */}
+        <form className="form" onSubmit={(e) => asyncPostCall(e)}>
+          {/*//////////////////////// EMAIL ///////////////////*/}
           <label>email</label>
           <input
             type="email"
             placeholder="Enter your email"
             required
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
+            defaultValue=""
+            // value={email}
+            onChange={(e) => validateEmail(e)}
           />
-          {emailIsNotValid.isValid && <p>{emailIsNotValid.message}</p>}
-          {/* PASSWORD */}
+          {renderEmailError()}
+
+          {/*//////////////////////// PASSWORD ///////////////////*/}
           <label>Password</label>
           <input
             type="password"
             required
             placeholder="Enter your password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
+            defaultValue=""
+            onChange={(e) => validatePassword(e)}
           />
-          {passwordIsNotValid.isValid && <p>{passwordIsNotValid.message}</p>}
-          <button className='submit' type="submit">login</button>
+          {renderPasswordError()}
+
+          {/*//////////////////////// SUBMIT ///////////////////*/}
+          <button className="submit" type="submit">
+            login
+          </button>
         </form>
       </div>
     </div>
